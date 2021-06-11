bc13e972ef571797402962d3c17cb2659d2bfb3f
Dhruti Mehta <84759440+dhruti1672@users.noreply.github.com>
2021-06-11 15:03:03 +0530

    Update navbar.php

--- admin/themepart/navbar.php
+++ admin/themepart/navbar.php
@@ -15,10 +15,7 @@
               <img src="images/faces/face28.jpg" alt="profile"/>
             </a>
             <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
-              <a class="dropdown-item">
-                <i class="ti-settings text-primary"></i>
-                Settings
-              </a>
+              
                 <a class="dropdown-item" href="logout.php">
                 <i class="ti-power-off text-primary"></i>
                 Logout
